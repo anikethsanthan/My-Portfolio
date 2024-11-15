b4bd16dcd2d5668bb3d202da87f473b65c6a9b9d
anikethsanthan <anikethsanthan999@gmail.com>
2024-11-15 10:42:34 +0530

    refactored env file

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,14 +3,13 @@ import "firebase/compat/firestore";
 
 const firebaseApp = firebase.initializeApp ({
   apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
+  authDomain: process.env.REACT_APP_authDomain ,
   databaseURL: process.env.REACT_APP_databaseURL,
-  projectId: process.env.REACT_APP_projectId,
+  projectId:process.env.REACT_APP_projectId ,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId,
+  measurementId: process.env.REACT_APP_measurementId
 });
-
 
 export const db=firebaseApp.firestore();
